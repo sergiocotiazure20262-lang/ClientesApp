1f0cca58eeb7eb7e93b58dcd11489ab4c5958cf7
sergiocotiazure20262-lang <sergiocotiazure20262@gmail.com>
2026-05-22 20:45:01 -0300

    Ajusta a aplicação para escutar a porta 8080

--- ClientesApp.API/Program.cs
+++ ClientesApp.API/Program.cs
@@ -2,6 +2,8 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
